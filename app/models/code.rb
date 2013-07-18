83b8d7ad5160eed8ec921504f212a03dbf2aad8b
Pablo Stuardo <pablo.stuardo.diaz@gmail.com>
2013-07-18 11:24:30 -0400

    cvreacion modelo codigos

---
+++ app/models/code.rb
@@ -0,0 +1,3 @@
+class Code < ActiveRecord::Base
+  attr_accessible :number, :status
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WsMmWanted::Application.config.secret_token = '780a8017dc7912a0c00815f61866dbae999165109e42f60bdcae81231b2d4571d4b98144cb64006cab71aedf879aecaa88bd808400c73ee2ac61027c2554db34'
