75950ab210976039805dfd14fed8cb4235ff5f8d
lortega <luis.ortega.araneda@gmail.com>
2013-07-26 14:13:01 -0400

    Se corrige el thread que agrega premios

--- config/initializers/auto_add.rb
+++ config/initializers/auto_add.rb
@@ -2,14 +2,11 @@ Thread.new{
   while true
     begin
       Prize.add_new_prize_entradas
-      print "INFO: ACABO DE AGREGAR UN PREMIO\n"
+      print "INFO: SE HA AGREGADO UN PREMIO\n"
     rescue
-      print "INFO: NO ESTOY AGREGANDO PREMIOS\n"
+      print "INFO: NO PUDE AGREGAR UN PREMIO\n"
     ensure
-      #for i in 0..2
       sleep(1.hours)
-      #print "INFO: ESTOY DURMIENDO POR 60 SEGUNDOS\n"
-      #end
     end
   end 
 }
