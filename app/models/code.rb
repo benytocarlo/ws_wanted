527dc29b831ad1577e349e7ce578ceb7caddb404
lortega <luis.ortega.araneda@gmail.com>
2013-07-31 13:29:41 -0400

    Se crea el atributo FacebookID y se el metodo para contar codigos inactivos

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,3 +1,7 @@
 class Code < ActiveRecord::Base
-  attr_accessible :description, :estado
+  attr_accessible :description, :activo, :facebook_id
+  
+  def self.count_inactives
+    Code.find(:all, :conditions => ["activo = ?", false]).count
+  end
 end
--- db/migrate/20130731154009_create_codes.rb
+++ db/migrate/20130731154009_create_codes.rb
@@ -2,7 +2,8 @@ class CreateCodes < ActiveRecord::Migration
   def change
     create_table :codes do |t|
       t.string :description
-      t.boolean :estado
+      t.boolean :activo
+      t.string :facebook_id
 
       t.timestamps
     end
