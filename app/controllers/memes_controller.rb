2cff8b9e9d5e29a2b48fd865a2cdadc967d31716
benytocarlo <itoknot2004@gmail.com>
2013-08-27 13:17:55 -0400

    convierto a string

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -7,8 +7,8 @@ class MemesController < ApplicationController
   
   # Este método imprime una imagen y la guarda en el FTP de Memefactory
   def print_meme
-    @dir_imagen = params[:facebook_id]
-    direccion_imagen = 'http://appdigital.cl/test_jquery/edit_image/screenshot2.php?id_insert=' + @dir_imagen
+    @dir_imagen = params[:idmeme]
+    direccion_imagen = 'http://appdigital.cl/test_jquery/edit_image/screenshot2.php?id_insert=' + @dir_imagen.to_s
     @originalimage = Magick::Image.read(direccion_imagen).first
     @originalimage.crop!(params[:x1].to_i, params[:y1].to_i, params[:w].to_i, params[:h].to_i)
     @originalimage.resize_to_fit!(810, 600)
