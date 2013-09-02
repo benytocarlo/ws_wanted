70277d19b89e05a8e104ab655068289bc4e76e4d
benytocarlo <itoknot2004@gmail.com>
2013-09-02 13:33:28 -0400

    cambio ruta

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -8,7 +8,7 @@ class MemesController < ApplicationController
   # Este método imprime una imagen y la guarda en el FTP de Memefactory
   def print_meme
     @dir_imagen = params[:idmeme]
-    direccion_imagen = 'http://www.unicayya.com/generadormeme/screenshot2.php?id_insert=' + @dir_imagen.to_s
+    direccion_imagen = 'http://www.unicayya.com/memefactory/screenshot2.php?id_insert=' + @dir_imagen.to_s
     kit = IMGKit.new(direccion_imagen, :quality => 100)
     @originalimage = kit.to_img(:jpg)
     @originalimage = Magick::Image.from_blob(@originalimage).first
